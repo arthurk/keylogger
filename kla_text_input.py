596952f80b79583345751ef60352f78798c52d75
Arthur Koziel <arthur@arthurkoziel.com>
2021-03-26 19:02:22 +0800

    handle shift key values in kla script

--- kla_text_input.py
+++ kla_text_input.py
@@ -15,20 +15,51 @@ parser = argparse.ArgumentParser()
 parser.add_argument('filename', type=str)
 args = parser.parse_args()
 
+shift_map = {
+    "/": "?",
+    ".": ">",
+    ",": "<",
+    "[": "{",
+    "]": "}",
+    "\\": "|",
+    "`": "~",
+    "1": "!",
+    "2": "@",
+    "3": "#",
+    "4": "$",
+    "5": "%",
+    "6": "^",
+    "7": "&",
+    "8": "*",
+    "9": "(",
+    "0": ")",
+    "-": "_",
+    "=": "+",
+    ";": ":",
+    "'": "\"",
+}
+
+# translates a key to it's value when shift mod is pressed
+def get_shift_value(key):
+    try:
+        return shift_map[key]
+    except KeyError:
+        return key.upper()
+
 with open(args.filename) as csvfile:
     r = csv.reader(csvfile)
     word = ""
-    for key, row in r:
+    for key, mod in r:
         if len(key) == 1:
+            if mod == "shift ":
+                key = get_shift_value(key)
             word += key
         elif key == "SPACE":
             word += " "
-        # word is longer than 1 char; SPACE, LEFT_ARROW, etc.
         else:
-            # print with newline
             if key == "ENTER":
                 print(word)
-            # print without newline
             else:
+                # print without newline
                 print(word, end="")
             word = ""
